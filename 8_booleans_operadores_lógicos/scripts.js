245aeaf3418e8d3d61785b782fb2e81c7b2018f3
tiago rodrigues <tiago.rodriguesfr1@gmail.com>
2022-10-27 10:20:01 -0300

    2 Seção concluida ,Tipos de dados JavaScript

---
+++ 8_booleans_operadores_lógicos/scripts.js
@@ -0,0 +1,7 @@
+console.log(5 > 3 && 3==2);
+console.log(5 > 3 || 3==1);
+console.log(3 == 3 && "Tiago" ==  "Tiago");
+console.log("Felipe" == "joao" || false);
+console.log(!(true && true));
+console.log(true && true);
+console.log(false || false);
